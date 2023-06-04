ba2c173757dcfb1f812cbbeee6b96ddfe43032dc
[moha-is] <[muhannadH.1420@gmail.com]>
2023-06-04 17:30:58 +0300

    answer

---
+++ signup.js
@@ -0,0 +1,11 @@
+let result = document.querySelector("#result");
+
+
+
+
+const signup = () => {
+    let FName = document.querySelector("#FName").value;
+    let LName = document.querySelector("#LName").value;
+
+    result.innerHTML = `!Congratulations ${FName}  ${LName}  you are now signed up`
+}
